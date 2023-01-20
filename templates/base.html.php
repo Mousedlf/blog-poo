d743b52b33f3a46f90dfed0e4013354a7e428a62
caroline <carolinedlf@gmail.com>
2023-01-20 10:50:47 +0100

    truc

--- templates/base.html.php
+++ templates/base.html.php
@@ -16,7 +16,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php?type=post&action=create">New Post</a>
+                    <a class="nav-link" href="index.php?type=post&action=create">New  Post</a>
                 </li>
             </ul>
             <form class="d-flex" role="search">
